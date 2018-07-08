a96d1cb1036752465fd4b791374f1001ba6eaad8
sleepingstanley <stanleyfkerr@gmail.com>
2018-07-08 06:57:42 -0700

    I had only messed up partially before, so now I have to fix that too.

--- app.js
+++ app.js
@@ -7,21 +7,18 @@ const passportConfig = require('./lib/passport');
 const config = require('./config');
 
 const app = express();
-//app.use(express.static('./server/static/'));
-//app.use(express.static('./client/dist/'));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-// LOAD MODELS HERE
 require('./models').connect(config.mongoURI).then(() => {
   passportConfig.init(passport);
 });
 
-app.use('/api/emails', './routes/api/emails');
-app.use('/api/auth', './routes/api/auth');
+app.use('/api/emails', require(path.join(__dirname, './routes/api/emails')));
+app.use('/api/auth', require(path.join(__dirname, './routes/api/auth')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(('client/build')));
+  app.use(express.static('./client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
