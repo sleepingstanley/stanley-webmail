4081727f984420a788e62b6fa139d30fd438a07c
sleepingstanley <stanleyfkerr@gmail.com>
2018-07-12 12:12:45 -0700

    Create npmrc file on the fly so we can fill with variables

---
+++ set-npmrc.js
@@ -0,0 +1,6 @@
+if(process.env.NODE_ENV !== 'production') return;
+  
+const fs = require('fs');
+fs.writeFileSync('client/.npmrc', '@fortawesome:registry=https://npm.fontawesome.com/');
+fs.appendFileSync('client/.npmrc', `//npm.fontawesome.com/:_authToken=${process.env.FONTAWESOME_AUTH}`);
+fs.chmodSync('client/.npmrc', 0600);
